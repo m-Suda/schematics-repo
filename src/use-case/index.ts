d34f7337eb580cffade1499f4f4bfad7ed0a74b0
masaks <oicwam.orz1228@gmail.com>
2020-05-04 16:11:00 +0900

    Initial Commit

---
+++ src/use-case/index.ts
@@ -0,0 +1,17 @@
+import { apply, applyTemplates, mergeWith, move, Rule, SchematicContext, url } from '@angular-devkit/schematics';
+import { normalize, strings } from "@angular-devkit/core";
+
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export default function (_options: any): Rule {
+    return (_, _context: SchematicContext) => {
+        return mergeWith(apply(url('./files'), [
+            applyTemplates({
+                ...strings,
+                name: _options.name,
+            }),
+            move(normalize(_options.path as string))
+        ]))
+    };
+}
